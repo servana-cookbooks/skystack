aad367473062b2073204540b832adf5b8b6fee44
Tass Skoudros <tass@theappbusiness.com>
2012-11-16 11:17:28 +0000

    testing git_ssh_wrapper vs ssh_wrapper
    Signed-off-by: Tass Skoudros <tass@theappbusiness.com>

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -35,7 +35,7 @@ app = node['deploy']
     group app['group']
     deploy_to app['path']
     action deploy_action
-    ssh_wrapper app['ssh_wrapper']
+    git_ssh_wrapper app['ssh_wrapper']
     shallow_clone true
     purge_before_symlink([])
     create_dirs_before_symlink([])
